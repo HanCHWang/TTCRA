c655b1bf287b84103a167f504021c5bdbc441aed
lalina3813 <lalina3813@ENGS-28580.eng.ox.ac.uk>
2021-03-12 18:36:51 +0000

    update

--- fmincon_QAP.m
+++ fmincon_QAP.m
@@ -3,9 +3,9 @@ clc
 clear all
 N=10;
 
-W=100*rand(N^2,N^2);
-
-mu=max(eig(W));
+% W=100*rand(N^2,N^2);
+load('W');
+mu=min(real(eig(W)));
 
 A_eq=[kron(eye(N),ones(1,N));kron(ones(1,N),eye(N))];
 
@@ -18,4 +18,4 @@ init_tmp=1/N.*ones(N^2,1);
 
 options = optimoptions('fmincon','Algorithm','sqp','MaxFunctionEvaluations',10000000,'MaxIterations',10000000);
 [y,f]=fmincon(@(X)QAP(X,W,mu),init_tmp,A,b,A_eq,b_eq,[],[],[],options);
-y=reshape(y,N,N);
+f
