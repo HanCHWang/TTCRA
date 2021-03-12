e4b55d9ae0ed518f6ed7a7b8dc1536fe92a2a68f
lalina3813 <lalina3813@ENGS-28580.eng.ox.ac.uk>
2021-03-12 11:35:07 +0000

    use fmincon with SQP solve QAP

---
+++ fmincon_QAP.m
@@ -0,0 +1,21 @@
+%using fmincon with SQP solver to solve convex relaxed QAP problem
+clc
+clear all
+N=10;
+
+W=100*rand(N^2,N^2);
+
+mu=max(eig(W));
+
+A_eq=[kron(eye(N),ones(1,N));kron(ones(1,N),eye(N))];
+
+b_eq=ones(2*N,1);
+
+A=-diag(ones(N^2,1));
+b=zeros(N^2,1);
+
+init_tmp=1/N.*ones(N^2,1);
+
+options = optimoptions('fmincon','Algorithm','sqp','MaxFunctionEvaluations',10000000,'MaxIterations',10000000);
+[y,f]=fmincon(@(X)QAP(X,W,mu),init_tmp,A,b,A_eq,b_eq,[],[],[],options);
+y=reshape(y,N,N);
